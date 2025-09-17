b4b6f88d08a859290da74955a4272cb5aa61979e
Melanie Jannielli <98432127+MelaJanni@users.noreply.github.com>
2025-09-16 21:30:37 -0300

    Update AdminPanelProvider.php

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -18,6 +18,12 @@ use App\Http\Middleware\MockAuth;
 
 class AdminPanelProvider extends PanelProvider
 {
+    public function boot(): void
+    {
+        // Configurar idioma para Filament
+        app()->setLocale('es');
+    }
+
     public function panel(Panel $panel): Panel
     {
         return $panel
@@ -33,7 +39,6 @@ class AdminPanelProvider extends PanelProvider
             ->sidebarCollapsibleOnDesktop()
             ->globalSearchKeyBindings(['command+k', 'ctrl+k'])
             ->spa()
-            ->locale('es')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
